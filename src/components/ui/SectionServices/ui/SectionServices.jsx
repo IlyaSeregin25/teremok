68c73fdab933cc58927c0030523eaaa9e2b2dabe
Ilya99 <Ilya99@mail.ru>
2025-09-03 20:36:52 +0400

    Fix bags and update styles

---
+++ src/components/ui/SectionServices/ui/SectionServices.jsx
@@ -0,0 +1,28 @@
+import ServiceCard from '@components/ui/ServiceCard';
+import styles from './style.module.css';
+import { SERVICES_LIST } from '@/constants';
+
+const SectionServices = () => {
+  return (
+    <section className={styles.services} aria-labelledby="services-title">
+      <div className={`${styles.services__inner} container`}>
+        <h2 className={styles.services__title} id="services-title">
+          Программы для детского развития
+        </h2>
+        <ul className={styles.services__list}>
+          {SERVICES_LIST?.map(service => {
+            return (
+              <li key={service.id} className={styles.services__item}>
+                <a href={service.url} className={styles.services__link}>
+                  <ServiceCard data={service} />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default SectionServices;
