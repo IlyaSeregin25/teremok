ae0b8b8673b9fd3e1a3571eafcfedc3f3102cb2d
Ilya99 <Ilya99@mail.ru>
2025-09-10 21:19:09 +0400

    Change styles on main page

--- src/components/ui/SectionAdvantages/ui/SectionAdvantages.jsx
+++ src/components/ui/SectionAdvantages/ui/SectionAdvantages.jsx
@@ -3,36 +3,121 @@ import AdvantagesList from '../../AdvantagesList';
 import ARROW_RIGHT_UP from '@assets/arrow-right-up.svg?react';
 import styles from './style.module.css';
 import { PATHS } from '@/constants';
+import BG_1 from '@assets/bg1.jpg';
+import BG_2 from '@assets/bg2.webp';
 
 const SectionAdvantages = () => {
   return (
-    <section className={styles.advantages}>
-      <div className={`${styles.advantages__inner} container`}>
-        <div className={styles.advantages__preview}>
-          <div className={styles.advantages__preview_main}>
-            <h2 className={styles.advantages__preview_title}>
-              Добро пожаловать в Теремок <br />
-              где таланты раскрываются свободно
-            </h2>
-            <div className={styles.advantages__preview_subtitle}>
-              <p>
-                Мы создаём яркие эмоции и надёжные знания. Начните свои первые шаги навстречу большому миру вместе с
-                нами!
-              </p>
+    <>
+      <section className={styles.advantages} style={{ backgroundImage: `url(${BG_1})` }}>
+        <div className={`${styles.advantages__inner} container`}>
+          <div className={styles.advantages__preview}>
+            <div className={styles.advantages__preview_main}>
+              <h2 className={styles.advantages__preview_title}>
+                Добро пожаловать
+                <br />в «Теремок» где таланты раскрываются свободно
+              </h2>
+              <div className={styles.advantages__preview_subtitle}>
+                <p>
+                  Мы создаём яркие эмоции и надёжные знания. Начните свои первые шаги навстречу большому миру вместе с
+                  нами!
+                </p>
+              </div>
+              <div className={styles.advantages__preview_advantages}>
+                <AdvantagesList />
+              </div>
+              <NavLink to={PATHS.aboutUs} className={styles.advantages__preview_link}>
+                Подробнее о нас <ARROW_RIGHT_UP width={16} height={16} />
+              </NavLink>
             </div>
-            <div className={styles.advantages__preview_advantages}>
-              <AdvantagesList />
-            </div>
-            <NavLink to={PATHS.aboutUs} className={styles.advantages__preview_link}>
-              Подробнее о нас <ARROW_RIGHT_UP width={16} height={16} />
-            </NavLink>
+          </div>
+          <div className={styles.advantages__body}>
+            <AdvantagesList />
           </div>
         </div>
-        <div className={styles.advantages__body}>
-          <AdvantagesList />
+      </section>
+      <section className={styles.advantages} style={{ backgroundImage: `url(${BG_1})` }}>
+        <div className={`${styles.advantages__inner} container ${styles.cl}`}>
+          <div className={styles.advantages__preview}>
+            <div className={styles.advantages__preview_main}>
+              <h2 className={styles.advantages__preview_title}>
+                Добро пожаловать
+                <br />в «Теремок» где таланты раскрываются свободно
+              </h2>
+              <div className={styles.advantages__preview_subtitle}>
+                <p>
+                  Мы создаём яркие эмоции и надёжные знания. Начните свои первые шаги навстречу большому миру вместе с
+                  нами!
+                </p>
+              </div>
+              <div className={styles.advantages__preview_advantages}>
+                <AdvantagesList />
+              </div>
+              <NavLink to={PATHS.aboutUs} className={styles.advantages__preview_link}>
+                Подробнее о нас <ARROW_RIGHT_UP width={16} height={16} />
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.advantages__body}>
+            <AdvantagesList />
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <section className={styles.advantages} style={{ backgroundImage: `url(${BG_2})` }}>
+        <div className={`${styles.advantages__inner} container`}>
+          <div className={styles.advantages__preview}>
+            <div className={styles.advantages__preview_main}>
+              <h2 className={styles.advantages__preview_title}>
+                Добро пожаловать
+                <br />в «Теремок» где таланты раскрываются свободно
+              </h2>
+              <div className={styles.advantages__preview_subtitle}>
+                <p>
+                  Мы создаём яркие эмоции и надёжные знания. Начните свои первые шаги навстречу большому миру вместе с
+                  нами!
+                </p>
+              </div>
+              <div className={styles.advantages__preview_advantages}>
+                <AdvantagesList />
+              </div>
+              <NavLink to={PATHS.aboutUs} className={styles.advantages__preview_link}>
+                Подробнее о нас <ARROW_RIGHT_UP width={16} height={16} />
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.advantages__body}>
+            <AdvantagesList />
+          </div>
+        </div>
+      </section>
+      <section className={styles.advantages} style={{ backgroundImage: `url(${BG_2})` }}>
+        <div className={`${styles.advantages__inner} container ${styles.cl}`}>
+          <div className={styles.advantages__preview}>
+            <div className={styles.advantages__preview_main}>
+              <h2 className={styles.advantages__preview_title}>
+                Добро пожаловать
+                <br />в «Теремок» где таланты раскрываются свободно
+              </h2>
+              <div className={styles.advantages__preview_subtitle}>
+                <p>
+                  Мы создаём яркие эмоции и надёжные знания. Начните свои первые шаги навстречу большому миру вместе с
+                  нами!
+                </p>
+              </div>
+              <div className={styles.advantages__preview_advantages}>
+                <AdvantagesList />
+              </div>
+              <NavLink to={PATHS.aboutUs} className={styles.advantages__preview_link}>
+                Подробнее о нас <ARROW_RIGHT_UP width={16} height={16} />
+              </NavLink>
+            </div>
+          </div>
+          <div className={styles.advantages__body}>
+            <AdvantagesList />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/ui/Socials/ui/Socials.jsx
+++ src/components/ui/Socials/ui/Socials.jsx
@@ -1,20 +1,29 @@
 import { DATA_FOR_COMMUNICATION } from '@/constants';
 import VKONTAKTE from '@assets/vk.svg?react';
+import TELEGRAM from '@assets/telegram.svg?react';
 import styles from './style.module.css';
 
 const Socials = () => {
-  const { vk } = DATA_FOR_COMMUNICATION.socials;
+  const { vk, telegram } = DATA_FOR_COMMUNICATION.socials;
   return (
     <>
       {DATA_FOR_COMMUNICATION?.socials && (
         <ul className={styles.soc1als_list}>
           {vk && (
-            <li key={vk.path}>
-              <a href={vk.path} target="_blank" className={styles.soc1als__link}>
-                <VKONTAKTE width={40} height={40} />
-                Вконтакте
-              </a>
-            </li>
+            <>
+              <li key={vk.path}>
+                <a href={vk.path} target="_blank" className={styles.soc1als__link}>
+                  <VKONTAKTE width={40} height={40} />
+                  Вконтакте
+                </a>
+              </li>
+              <li key={telegram.path}>
+                <a href={telegram.path} target="_blank" className={styles.soc1als__link}>
+                  <TELEGRAM width={40} height={40} />
+                  Телеграм
+                </a>
+              </li>
+            </>
           )}
         </ul>
       )}
